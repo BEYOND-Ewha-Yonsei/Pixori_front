08b7c8db560548adca543a39ed08f19c066dc772
yewonbahn <banyaewon98@gmail.com>
2021-07-25 23:07:40 +0900

    maintitle error fix

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import {BrowserRouter as Router,Redirect,Route,Switch} from "react-router-dom";
 import Home from "../Routes/Home";
 import Collection from "../Routes/Collection"
@@ -6,11 +6,55 @@ import Collection2 from "../Routes/Collection2"
 import Maker from "../Routes/Maker";
 import Menubar from "./Menubar";
 import Footer from "./Footer";
+import profileicon from "../img/ic_purple.svg"
+import Modal2 from '../Components/Modal2';
+import {InitCluster} from "../clusters/init-cluster"
+import {ProfileCluster} from '../clusters/profile-cluster'
+import React, {  useState } from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/home.css'
+import {useCurrentUser} from "../hooks/current-user"
+
+import "../config"
+import {AuthCluster} from '../clusters/auth-cluster';
+import {RecoilRoot} from "recoil"
+import {CurrentUserSubscription} from "../hooks/current-user"
+import { Button } from '@material-ui/core';
+
+function Init() {
+
+  const [ modalOpen, setModalOpen ] = useState(false);
+  const openModal = () => {
+    setModalOpen(true);
+}
+const closeModal = () => {
+    setModalOpen(false);
+}
+
+  const cu = useCurrentUser()
+
+  return (
+    <div>
+
+    <Button  onClick={openModal} className="profileicon" >
+    <img src={profileicon} ></img>
+   </Button>
+   <Modal2 open={ modalOpen } close={ closeModal } header="" address={cu.addr}  >
+      </Modal2></div>
+  )
+}
+
+
 export default ()=>(
+
+
     <Router>
        <>
 
       <Menubar />
+      <AuthCluster className="auth"/>
+      <CurrentUserSubscription />
+      <Init />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/maker" component={Maker} />
--- src/index.js
+++ src/index.js
@@ -1,54 +1,16 @@
 import React, {  useState } from 'react';
 import ReactDOM from 'react-dom';
-import './styles/home.css'
+
 import App from './Components/App';
 
-import "./config"
-import {AuthCluster} from './clusters/auth-cluster';
+
 import {RecoilRoot} from "recoil"
-import {CurrentUserSubscription} from "./hooks/current-user"
-import { Button } from '@material-ui/core';
-import {InitCluster} from "./clusters/init-cluster"
-import {ProfileCluster} from './clusters/profile-cluster'
-import {useCurrentUser} from "./hooks/current-user"
-import { Fragment } from 'react';
-import profileicon from "./img/ic_purple.svg"
-import Modal2 from './Components/Modal2';
-
-function Init() {
-
-  const [ modalOpen, setModalOpen ] = useState(false);
-  const openModal = () => {
-    setModalOpen(true);
-}
-const closeModal = () => {
-    setModalOpen(false);
-}
-
-  const cu = useCurrentUser()
-
-
-  return (
-    <div>
-
-    <Button  onClick={openModal} className="profileicon" >
-    <img src={profileicon} ></img>
-   </Button>
-   <Modal2 open={ modalOpen } close={ closeModal } header="" address={cu.addr}  >
-      </Modal2></div>
-  )
-}
-
 
 
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <AuthCluster className="auth"/>
-      <CurrentUserSubscription />
-      <Init />
- 
       <App />
     </RecoilRoot>
   </React.StrictMode>, document.getElementById('root'));
