6e738e7324384b27cfb082dbe7c9d6a0a05affbc
kwklly <yeonlee@i.ewha.ac.kr>
2021-08-01 01:37:09 +0900

    :building_construction: Move init-cluster

--- src/Components/Modal2.js
+++ src/Components/Modal2.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import "../styles/smodal.css";
 import { useCurrentUser } from "../hooks/current-user"
+import { InitCluster } from "../clusters/init-cluster"
 import { fetchProfile } from "../flow/fetch-profile.script"
 
 const Modal2 = (props) => {
@@ -10,37 +11,63 @@ const Modal2 = (props) => {
     const [profile, setProfile] = useState("Your Name")
 
     useEffect(() => {
-        async function refetch() {
-            await fetchProfile(cu.addr)
-                .then(profile => {
-                    if (profile == null) return profile
-                    return profile
-                })
-                .then(setProfile)
-        }
-        refetch();
-    })
+            async function refetch() {
+                await fetchProfile(cu.addr)
+                    .then(profile => {
+                        if (profile == null) return profile
+                        return profile
+                    })
+                    .then(setProfile)
+            }
+            refetch();
+        })
 
     if (cu.addr) {
-        return (
-            <div className={open ? 'openModal modal2' : 'modal2'}>
-                {open ? (
-                    <section>
-                        <header>
-                            {header}
-                            <button className="close" onClick={close}> &times; </button>
-                        </header>
-                        <main>
-                            <p className="profileName">{profile.name}</p>
-                            <p style={{ marginBottom: 25 }}><Link to="/profile" className="editProfile">Edit profile</Link></p>
-                            <p><Link to="/collection2" className="editProfile">My collection</Link></p>
-                        </main>
-                        <footer>
-                        </footer>
-                    </section>
-                ) : null}
-            </div>
-        )
+        if (profile) { // init 했으면 profile 생성. profile.name은 아직 없음
+            return (
+                <div className={open ? 'openModal modal2' : 'modal2'}>
+                    {open ? (
+                        <section>
+                            <header>
+                                {header}
+                                <button className="close" onClick={close}> &times; </button>
+                            </header>
+                            <main>
+                                <p className="profileName">{profile.name}</p>
+                                <p style={{ marginBottom: 25 }}><Link to="/profile" className="editProfile">Edit profile</Link></p>
+                                <p><Link to="/collection2" className="editProfile">My collection</Link></p>
+                                <button className="logOut" onClick={cu.logOut}>Log out</button>
+                            </main>
+                            <footer>
+                            </footer>
+                        </section>
+                    ) : null}
+                </div>
+            )
+        } else {
+            return (
+                <div className={open ? 'openModal modal2' : 'modal2'}>
+                    {open ? (
+                        <section>
+                            <header>
+                                {header}
+                                <button className="close" onClick={close}> &times; </button>
+                            </header>
+                            <main>
+                                <p className="note">You must initialize your account in order to set an ID.</p>
+                                <InitCluster address={cu.addr} />
+                            </main>
+                            <footer>
+                            </footer>
+                        </section>
+                    ) : null}
+                </div>
+
+
+
+            )
+        }
+
     } else {
         return (
             <div className={open ? 'openModal modal2' : 'modal2'}>
--- src/clusters/init-cluster.js
+++ src/clusters/init-cluster.js
@@ -1,40 +1,35 @@
-import React, {useEffect} from "react"
-import {useCurrentUser} from "../hooks/current-user"
-import {useInit} from "../hooks/init"
+import React, { useEffect } from "react"
+import { useCurrentUser } from "../hooks/current-user"
+import { useInit } from "../hooks/init"
 import '../styles/home.css';
 
-const fmtBool = bool => (bool ? "yes" : "no")
+export function InitCluster({ address }) {
 
-export function InitCluster({address}) {
+  const init = useInit(address)
+  const cu = useCurrentUser()
+  //useEffect(() => init.check(), [address])
 
-const init = useInit(address)
-const cu = useCurrentUser()
-//useEffect(() => init.check(), [address])
+  useEffect(() => {
+    async function refetch() {
+      init.check();
+    }
+    refetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [address])
 
-useEffect(() => {
-  async function refetch(){
-    init.check();
-  }
-  refetch();
-// eslint-disable-next-line react-hooks/exhaustive-deps
-}, [address])
-
- if (address == null) return null
- return (
-  <div className="initCluster">
-    <h3>My Address: {address}</h3>
-    <ul>
-      <li>
-      <strong>Init Profile?: </strong>
-         {init.isIdle && <span>{fmtBool(init.profile)}</span>}
-         {!init.profile && cu.addr === address && init.isIdle && (
-           <button disabled={init.isProcessing} onClick={init.exec}>
-             Initialize Profile
-           </button>
-         )}
-        {init.isProcessing && <span>PROCESSING</span>}
-       </li>
-     </ul>
-   </div>
-)
+  if (address == null) return null
+  return (
+    <div className="initCluster">
+      <ul>
+        <li>
+          {!init.profile && cu.addr === address && init.isIdle && (
+            <button className="initButton" disabled={init.isProcessing} onClick={init.exec}>
+              Initialize account
+            </button>
+          )}
+          {init.isProcessing && <span>PROCESSING</span>}
+        </li>
+      </ul>
+    </div>
+  )
 }
