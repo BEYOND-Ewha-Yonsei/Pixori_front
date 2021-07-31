f0cc5e6bbf52aff5666982a6e604fffd9e87f1dc
kwklly <yeonlee@i.ewha.ac.kr>
2021-07-31 13:12:42 +0900

    :construction: Disable marketplace

--- src/clusters/token-cluster.js
+++ src/clusters/token-cluster.js
@@ -1,36 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
+/* import React, { useEffect, useState } from "react";
 import * as fcl from "@onflow/fcl";
 import * as t from "@onflow/types"
+import {useCurrentUser} from '../hooks/current-user' */
 
 export function TokenCluster({addresss, address}) {
-  const [nftInfo, setNftInfo] = useState(null)
-  const fetchTokenData = async () => {
-    const encoded = await fcl
-      .send([
-        fcl.script`
-        import Monday from 0xdb16a5e14c410280
-
-        pub fun main(addresss: Address, address: String): [AnyStruct] {
-          let nftOwner = getAccount(addresss)  
-          let capability = nftOwner.getCapability<&{Monday.NFTReceiver}>(/public/MondayReceiver)
-          let receiverRef = capability.borrow()
-              ?? panic("Could not borrow the receiver reference")
-      
-          return receiverRef.getMetadata(address: address)
-        }
-      `,
-      fcl.args([fcl.arg(addresss, t.Address), fcl.arg(address, t.String)]),
-      ])
-    
-    const decoded = await fcl.decode(encoded)
-    setNftInfo(decoded) 
-  };
+  /* const [nftInfo, setNftInfo] = useState(null)
+  const cu = useCurrentUser()
+  useEffect(() => {
+    const fetchTokenData = async () => {
+      const encoded = await fcl
+        .send([
+          fcl.script`
+          import Monday from 0xdb16a5e14c410280
   
-  fetchTokenData();
+          pub fun main(addresss: Address, address: String): [AnyStruct] {
+            let nftOwner = getAccount(addresss)  
+            let capability = nftOwner.getCapability<&{Monday.NFTReceiver}>(/public/MondayReceiver)
+            let receiverRef = capability.borrow()
+                ?? panic("Could not borrow the receiver reference")
+            return receiverRef.getMetadata(address: address)
+          }
+        `,
+        fcl.args([fcl.arg(addresss, t.Address), fcl.arg(address, t.String)]),
+        ])
+      
+      const decoded = await fcl.decode(encoded)
+      setNftInfo(decoded) 
+    };
+    fetchTokenData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cu.addr]) */
 
   return (
-    <div>
-      {
+    <div>Not supported yet
+      {/* {
         nftInfo &&
         <div>
             {Object.keys(nftInfo).map(k => {
@@ -42,7 +46,7 @@ export function TokenCluster({addresss, address}) {
             })
             }
         </div>
-      }
+      } */}
     </div>
   );
 }
