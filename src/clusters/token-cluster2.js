a169471f77e12e801ad28215e124482b7f49b707
kwklly <yeonlee@i.ewha.ac.kr>
2021-07-31 13:15:18 +0900

    :recycle: Refactor code

--- src/clusters/token-cluster2.js
+++ src/clusters/token-cluster2.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as fcl from "@onflow/fcl";
 import * as t from "@onflow/types"
+import { useCurrentUser } from '../hooks/current-user'
 import Collection2 from "../Routes/Collection2"
 import "../styles/market.css"
-import playbutton from "../img/btn_play_circle_purple.png"
-export function TokenCluster2({addresss, address}) {
+
+export function TokenCluster2({ addresss, address }) {
   const [nftInfo, setNftInfo] = useState(null)
-  const fetchTokenData = async () => {
-    const encoded = await fcl
-      .send([
-        fcl.script`
+  const cu = useCurrentUser()
+  const colorArr = [];
+  useEffect(() => {
+    const fetchTokenData = async () => {
+      const encoded = await fcl
+        .send([
+          fcl.script`
         import Monday from 0xdb16a5e14c410280
 
         pub fun main(addresss: Address, address: String): [AnyStruct] {
@@ -21,15 +25,15 @@ export function TokenCluster2({addresss, address}) {
           return receiverRef.getMetadata(address: address)
         }
       `,
-      fcl.args([fcl.arg(addresss, t.Address), fcl.arg(address, t.String)]),
-      ])
+          fcl.args([fcl.arg(addresss, t.Address), fcl.arg(address, t.String)]),
+        ])
 
-    const decoded = await fcl.decode(encoded)
-    setNftInfo(decoded) 
-  };
-
-  const colorArr = new Array();
-  fetchTokenData();
+      const decoded = await fcl.decode(encoded)
+      setNftInfo(decoded)
+    };
+    fetchTokenData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cu.addr])
 
 
   return (
@@ -37,19 +41,19 @@ export function TokenCluster2({addresss, address}) {
       {
         nftInfo &&
         <div className="">
-            {Object.keys(nftInfo).map(k => {
-                colorArr[k] = nftInfo[k].color
-              return (
-                <div className="card_marketplace" >
+          {Object.keys(nftInfo).map(k => {
+            colorArr[k] = nftInfo[k].color
+            return (
+              <div className="card_marketplace" >
                 <p className="NftName">
-                  NFT Name: {nftInfo[k].name} <br/> # :{k}  </p>  <Collection2 arr1={colorArr[k]} />
-                </div>
-              )
-            })
-            }
+                  NFT #{parseInt(k)+1}: {nftInfo[k].name}  </p>  <Collection2 arr1={colorArr[k]} />
+              </div>
+            )
+          })
+          }
         </div>
       }
     </div>
   );
 
-} 
+}
