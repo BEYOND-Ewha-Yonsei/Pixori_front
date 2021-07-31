54b66b78f66542ef73158aa7fe31ef04402c14b5
kwklly <yeonlee@i.ewha.ac.kr>
2021-07-31 15:38:24 +0900

    :lipstick: Modify footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,31 +1,18 @@
 import React from "react";
-
 import '../styles/home.css';
 
-
-
-
-function Footer (){
+function Footer() {
 
   return (
-
-
     <div className="Rectangle-467">
-  <div className ="Customer-services">Customer services</div>
-  <div className ="Follow-us">Follow us</div>
-
-  <h4 className="Contact">Terms of service   &nbsp;&nbsp;|  &nbsp;&nbsp; Help&nbsp; &nbsp;| &nbsp; &nbsp;FAQs &nbsp;&nbsp; | &nbsp; &nbsp;Contact</h4>
-
-
-
-
-
-
-
+      <div className="Customer-services">Customer services</div>
+      <div className="Follow-us">Follow us</div>
+      <h4 className="Contact">Terms of service   &nbsp;&nbsp;|  &nbsp;&nbsp; Help&nbsp; &nbsp;| &nbsp; &nbsp;FAQs &nbsp;&nbsp; | &nbsp; &nbsp;Contact</h4>
+      <h4 className="Contact">© Pixori. All rights reserved.</h4>
     </div>
   )
-  
+
 }
-   
+
 
 export default Footer
